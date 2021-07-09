bbb6610ca31df2a1646f6bcc75aa305b671f886a
kaidegit <2857693944@qq.com>
2021-07-09 15:50:01 +0800

    add speed read func

---
+++ Core/Inc/speed.h
@@ -0,0 +1,12 @@
+//
+// Created by yekai on 2021/7/9.
+//
+
+#ifndef ROTARYINVERTEDPENDULUM_SPEED_H
+#define ROTARYINVERTEDPENDULUM_SPEED_H
+
+#include "stm32f4xx.h"
+
+extern int32_t motor_speed;
+
+#endif //ROTARYINVERTEDPENDULUM_SPEED_H
---
+++ Core/Src/speed.c
@@ -0,0 +1,13 @@
+//
+// Created by yekai on 2021/7/9.
+//
+
+#include "speed.h"
+#include "tim.h"
+#include "stdbool.h"
+
+uint32_t count_start,count_end;
+int32_t motor_speed;
+bool testing;
+
+
