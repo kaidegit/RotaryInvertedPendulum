faa3a4279048cc8a5eb3de292e7fd0a1658c09d5
jtqq2607229429 <85485068+jtqq2607229429@users.noreply.github.com>
2021-07-09 16:25:28 +0800

    add pidcale

--- Core/Src/tim_irq_cb.c
+++ Core/Src/tim_irq_cb.c
@@ -17,10 +17,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
      */
     int out;
     int Measure;
+
     if (htim->Instance == htim13.Instance) {
         Measure=__HAL_TIM_GET_COUNTER(&htim4);
-        out=PID_calc(&Rp_PID,get_error(3300,Measure));
-        SetMotorSpeed(out);
+        if((Measure >= 2830) && (Measure <= 3300))
+            out=PID_calc(&Rp_PID,get_error(3300,Measure));
+            SetMotorSpeed(out);
     } else if (htim->Instance == htim14.Instance) {
 
     }
